f8066acfe489ed39a85733925f0006b0d1b7181c
Shogo Kagenaga <s.kagenaga@gmail.com>
2021-12-05 23:51:43 +0900

    2-2. 演算子とString型の足し算

--- app/controllers/StudyController.scala
+++ app/controllers/StudyController.scala
@@ -9,18 +9,22 @@ class StudyController @Inject()(val controllerComponents: ControllerComponents) 
 
   def study = Action { _ =>
     // 2. 型
-    // 2-1. 初めての型
+    // 2-2. 演算子とString型の足し算
 
-    // 全てのデータ(値)には"型"というのが存在しています。
-    // 型というのは、とりあえず今は、データの形みたいなものと捉えてみましょう。
+    val message1: String = "はい。" // String(文字列)型
+    val message2 = "みなさん、こんばんは。" // String(文字列)型 (型アノテーションがなくても型がなくなることはあーりません。)
+
+    // String型とString型は足し算(?)できます。
+    // 以下は、変数message1と変数message2を足し算したものを変数messageに入れてます。
+    val message: String = message1 + message2
     
-    // 変数名等の右に`: 型名`という形で"型"を明示できます。型アノテーションって言うんだって。
-    // 文字列は実はString型なので、以下のような表記ができます。
-    val message: String = "こんちゃーす"
-    Ok(message).as(HTML)
+    // String型とString型を足すと、双方が結合された文字列になります。
+    Ok(message).as(HTML) // "はい。みなさん、こんばんは。"
     
-    // 「は？」「で？」と思うかもしれませんが、 
-    // 気にせず次にいきましょう。
+    // 今回`+`記号が出てきましたが、
+    // こんな感じで、計算を行うための記号等を"演算子"と呼んだりします。
+    // ちなみに変数に値を入れている`=`も代入演算子と呼ばれるような演算子の一種です。
+    // (※今まで隠していましたが変数に値を入れることを"代入"といったりします。ひみつやで🤫)
   }
 
 }
