d4889b15cc74a5dea31cc7b9d8c7cc7ea82500a2
Shogo Kagenaga <s.kagenaga@gmail.com>
2022-01-05 13:59:21 +0900

    6-5. 組込型の演算子とメソッド

--- app/controllers/StudyController.scala
+++ app/controllers/StudyController.scala
@@ -9,76 +9,41 @@ class StudyController @Inject()(val controllerComponents: ControllerComponents) 
 
   def study = Action { _ =>
     // 6. クラス
-    // 6-4. メソッド2
+    // 6-5. 組込型の演算子とメソッド
 
-    // 今回は特に大きな新しい話はなく、
-    // 追加の具体例と共に小ネタ等を紹介します。
-    // Coinsクラスにいくつか別のフィールド・メソッドを定義します。
-
-    class Coins(val fiveHundred: Int,
-                val oneHundred: Int,
-                val fifty: Int,
-                val ten: Int,
-                val five: Int,
-                val one: Int) {
-
-      val sum: Int = (500 * fiveHundred) + (100 * oneHundred) + (50 * fifty) + (10 * ten) + (5 * five) + one
-
-      // 中身のデータをHTML形式の文字列にして表した変数
-      val html: String = {
-        s"""
-           |500円玉: $fiveHundred 枚<br>
-           |100円玉: $oneHundred 枚<br>
-           |50円玉: $fifty 枚<br>
-           |10円玉: $ten 枚<br>
-           |5円玉: $five 枚<br>
-           |1円玉: $one 枚<br>
-           |<b>合計: $sum 円</b><br>
-           |""".stripMargin
-      }
-
-      // 足し算メソッド(一部の記号はメソッド名として利用できます。)
-      def +(c: Coins): Coins = {
-        new Coins(
-          fiveHundred + c.fiveHundred,
-          oneHundred + c.oneHundred,
-          fifty + c.fifty,
-          ten + c.ten,
-          five + c.five,
-          one + c.one)
-      }
-
-      // 引き算メソッド(一部の記号はメソッド名として利用できます。)
-      def -(c: Coins): Coins = {
-        new Coins(
-          fiveHundred - c.fiveHundred,
-          oneHundred - c.oneHundred,
-          fifty - c.fifty,
-          ten - c.ten,
-          five - c.five,
-          one - c.one)
-      }
-
-    }
-
-    // ここから使用例です。
-    val coins1 = new Coins(10, 10, 10, 10, 10, 10)
-    val coins2 = new Coins(1, 2, 3, 4, 5, 6)
+    // 以前よりInt型やString型を始めとした、
+    // 色々な型を見てきました。その際、例えば、
+    // Int型の足し算き算などを以下のように記載していたと思います。
+    val number1 = 1 + 2
     
-    // val addedCoins = coins1.+(coins2)
-    // 本来、上記のように書きますが、引数が1つだけの場合は以下のように書けるという特殊ルールがScalaにはあります。
-    val addedCoins = coins1 + coins2
-    val subtractedCoins = coins1 - coins2
+    // さて、前回Coinsには`+`というメソッドを定義して、
+    // 上記と似た感じに利用していました。
+    // 実は、Int型の`+`も逆に以下のように記載できます。
+    val number2 = 1.+(2)
     
-    // すこーしだけHTMLにも色気を出しています。
-    Ok(
-      s"""
-         |<h2>addedCoins</h2>
-         |${addedCoins.html}
-         |<hr>
-         |<h2>subtractedCoins</h2>
-         |${subtractedCoins.html}
-         |""".stripMargin).as(HTML)
+    // そうです？
+    // 実は全ての型の演算子は、
+    // 組込として初めから定義されてはいるものの、
+    // 実体は各型(クラス)のメソッド(やフィールド)だったのです(ドーン！👻)
+    
+    // ただし`1`や`2`等のリテラルは、
+    // Coinsと違って`new`してないですね？
+    // 組込型の全てではないですが、
+    // 組込型は利用頻度がとても多いものが多いので、
+    // Scala言語が、特別にわかりやすい、
+    // ある意味、特殊なインスタンス生成方法を用意しているのが、
+    // 各リテラル表記だったのです。
+    
+    // ※全てのプログラミング言語が同じ仕組みというわけではないのでご注意ください。
+    
+    // 前回以下と記載していましたが、
+    // > "そのクラスと関係の深い"処理を含めておけば、
+    // > 読みやすいプログラムに仕上げられる場合があります。
+    // 組み込みクラス・メソッドのこともおさらいしつつ、
+    // クラス・メソッドのお陰で非常にわかりやすく記述できているというのが、
+    // 多少でも体感・認識いただけると嬉しいなと思います。
+    
+    Ok("クラス最高！メソッド最高！").as(HTML)
 
   }
 
