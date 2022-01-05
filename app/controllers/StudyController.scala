cd67636406ef43643f27e930081bfb6279efc433
Shogo Kagenaga <s.kagenaga@gmail.com>
2022-01-05 12:10:24 +0900

    6-4. メソッド2

--- app/controllers/StudyController.scala
+++ app/controllers/StudyController.scala
@@ -9,21 +9,11 @@ class StudyController @Inject()(val controllerComponents: ControllerComponents) 
 
   def study = Action { _ =>
     // 6. クラス
-    // 6-3. メソッド
+    // 6-4. メソッド2
 
-    // クラスでは、前回の変数(フィールド)に加えて、
-    // 関数も中に含むことができます。
-    // 前々回の`exchange500For100`関数のように、
-    // クラスの中でなくても、当然定義できるのですが、
-    // クラスの中に、"そのクラスと関係の深い"処理を含めておけば、
-    // 読みやすいプログラムに仕上げられる場合があります。
-
-    // こういったクラス内の関数を、
-    // "メソッド"と呼びます。"メンバ関数"とも呼ばれたりします。
-    // フィールドと同様に、他にも、
-    // 単に"メンバ"・"プロパティ"などと呼ばれることもあるのでご注意ください。
-
-    // 例として、`exchange500For100`をメソッドとして定義してみましょう。
+    // 今回は特に大きな新しい話はなく、
+    // 追加の具体例と共に小ネタ等を紹介します。
+    // Coinsクラスにいくつか別のフィールド・メソッドを定義します。
 
     class Coins(val fiveHundred: Int,
                 val oneHundred: Int,
@@ -34,44 +24,60 @@ class StudyController @Inject()(val controllerComponents: ControllerComponents) 
 
       val sum: Int = (500 * fiveHundred) + (100 * oneHundred) + (50 * fifty) + (10 * ten) + (5 * five) + one
 
-      // これです。
-      def exchange500For100(): Coins = {
-        val exchanged100coins = fiveHundred * 5
-        new Coins(0, oneHundred + exchanged100coins, fifty, ten, five, one)
+      // 中身のデータをHTML形式の文字列にして表した変数
+      val html: String = {
+        s"""
+           |500円玉: $fiveHundred 枚<br>
+           |100円玉: $oneHundred 枚<br>
+           |50円玉: $fifty 枚<br>
+           |10円玉: $ten 枚<br>
+           |5円玉: $five 枚<br>
+           |1円玉: $one 枚<br>
+           |<b>合計: $sum 円</b><br>
+           |""".stripMargin
       }
 
-      // 前々回の`exchange500For100`の定義は以下です。
-      // 引数と`coins.`部分が上記ではなくなっています。
+      // 足し算メソッド(一部の記号はメソッド名として利用できます。)
+      def +(c: Coins): Coins = {
+        new Coins(
+          fiveHundred + c.fiveHundred,
+          oneHundred + c.oneHundred,
+          fifty + c.fifty,
+          ten + c.ten,
+          five + c.five,
+          one + c.one)
+      }
 
-      // def exchange500For100(coins: Coins): Coins = {
-      //   val exchanged100coins = coins.fiveHundred * 5
-      //   new Coins(0, coins.oneHundred + exchanged100coins, coins.fifty, coins.ten, coins.five, coins.one)
-      // }
+      // 引き算メソッド(一部の記号はメソッド名として利用できます。)
+      def -(c: Coins): Coins = {
+        new Coins(
+          fiveHundred - c.fiveHundred,
+          oneHundred - c.oneHundred,
+          fifty - c.fifty,
+          ten - c.ten,
+          five - c.five,
+          one - c.one)
+      }
 
     }
 
-    // クラス定義での`{}`の中で定義する以外は、
-    // 表記上のルールは今までの関数と変わりません。
-
-    // ただし、上記の例の通り、メソッドの場合は、
-    // 同じクラス内の他フィールドやメソッドを`.`等をつけずに
-    // 参照できるという特徴があります。
-    // これのお陰で、処理の内容次第ですが表記を簡素化することができます。
-
-    // メソッド使う例も見ていきましょう。
-    // 基本的にはフィールドと同様ですが、"()"や引数を指定する形になります。
-    val coins1 = new Coins(1, 2, 3, 4, 5, 6)
-    val coins2 = coins1.exchange500For100()
-        
+    // ここから使用例です。
+    val coins1 = new Coins(10, 10, 10, 10, 10, 10)
+    val coins2 = new Coins(1, 2, 3, 4, 5, 6)
+    
+    // val addedCoins = coins1.+(coins2)
+    // 本来、上記のように書きますが、引数が1つだけの場合は以下のように書けるという特殊ルールがScalaにはあります。
+    val addedCoins = coins1 + coins2
+    val subtractedCoins = coins1 - coins2
+    
+    // すこーしだけHTMLにも色気を出しています。
     Ok(
       s"""
-         |coins1.fiveHundred = ${coins1.fiveHundred}<br>
-         |coins1.oneHundred = ${coins1.oneHundred}<br>
-         |coins1.sum = ${coins1.sum}<br>
-         |<br>
-         |coins2.fiveHundred = ${coins2.fiveHundred}<br>
-         |coins2.oneHundred = ${coins2.oneHundred}<br>
-         |coins2.sum = ${coins2.sum}<br>
+         |<h2>addedCoins</h2>
+         |${addedCoins.html}
+         |<hr>
+         |<h2>subtractedCoins</h2>
+         |${subtractedCoins.html}
          |""".stripMargin).as(HTML)
 
   }
