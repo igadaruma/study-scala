e89f790486e1552fd03c0f51d456a4870409ef22
Shogo Kagenaga <s.kagenaga@gmail.com>
2022-01-08 01:37:00 +0900

    7-1. Option

--- app/controllers/StudyController.scala
+++ app/controllers/StudyController.scala
@@ -8,77 +8,91 @@ import javax.inject._
 class StudyController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
   def study = Action { _ =>
-    // 6. クラス
-    // 6-9. Any型・Object型
+    // 7. 組込クラスと高階関数とラムダ式
+    // 7-1. Option
 
-    // 今回は番外編です。
-    // よくわからんぞ？となった場合でも気にせず次に進んで問題ないです。
-    
-    // まずはAny型って何？という話です。
-    // おそらく、もう忘れちゃっていることと思いますが、
-    // 3-4にて登場していた型です。折角なのでぜひこの機会に復習してみてください。
+    // 今回は、Scalaプログラミングにおいて、
+    // とても重要なOption型に入門していきます。
+    // 初めはちょっと難しく感じると思いますが、
+    // 慣れれば簡単なので頑張っていきましょう。
 
-    // さて、このAny型は、
-    // 全ての型の先祖クラスである、という特別な存在です。
-    // よって、ヴェルタースオリジナルです(スルーしてください)。
-    // なんと自分で定義したクラスについても同じで、
-    // Anyをextendsしていなくても勝手にそうなるのです。
-    
-    // つまり以下のように、
-    // なんでもかんでもAny型の変数に入れることができます。
-    // ※関数の戻り値の型としても同様です。
-    
-    class Something() // 自分で定義したクラス
-    
-    val someAsAny: Any = new Something() // Any型の変数に入る。
-    val stringAsAny: Any = "string"
-    val intAsAny: Any = 10
-    val boolAsAny: Any = true
-    
-    // 一方で、Any型がでてきてしまうと、
-    // match等を使って型を判定する必要が多いので、
-    // 原則避けるべき型という捉え方が良いのではないかと思います。
-    
-    // そして、さらに、
-    // 類似したObjectという型(クラス)というのも存在します。
-    // 以下のように、おおよそAny型と同じなのですが、
-    // 数値型や真偽型はObject型ではないという罠もあります。
-    val someAsObj: Object = new Something() // 自分で定義したクラスは必ずObject型として扱えます。
-    val stringAsObj: Object = "string"
-    // val intAsObj: Object = 10 // エラー
-    // val boolAsObj: Object = true // エラー
-    
-    // この違いからも予想できるかもですが、
-    // AnyはObjectよりも上位なので、Object型の値をAny型の変数には入れられますが、
-    // 逆はだめです。
-    
-    // Object型には`toString`メソッド等がいくつか組込実装されています。
-    // なので、空っぽ定義のSomething型のインスタンスでも、
-    // toStringが呼び出せてしまいます。
-    // わお。
-    val someAsString = (new Something()).toString
-    
-    // ただし、Object型のtoStringが望ましい文字列を返さない事の方が多いと思われますので、
-    // むしろ、仮に同名のtoStringというメソッドを独自実装したい場合は、
-    // `override`する必要があるという点に注意しましょう。
-    
-    class SomethingGreat() {
-      // Object型で定義されているメンバはoverrideが必要
-      override def toString(): String = "偉大だわぁ"
-      
-      // ちなみに、`super.メンバ`で親クラスのメンバを呼び出せます。
-      def toStringByObject(): String = super.toString // Object型で実装されている`toString`を呼び出しています。
+    // まずは、型構造を解説します。
+    // Option型といいつつ、登場する型が3+1個あります。
+    // 1つ目: Option型 
+    // 2つ目: Some型
+    // 3つ目: None型
+    // (4つ目: Some型に含まれる値の型)
+    // です。
+
+    // Option型は6-7の抽象クラスになっています。
+    // そしてSome型とNone型がその子クラスになっています。
+    // 他に子クラスは存在しません。
+
+    // Option型は、ざっくり表現すると、
+    // "値が有るかもしれないし無いかもしれない型"です。
+    // 子クラスの、Some型は"値がある場合"、None型は"値がない場合"に対応します。
+    // よくわからないと思いますので、実際のソースを見ていきます。
+
+    // 例としまして、前述の"4つ目"の型がInt型のケースを考えてみます。
+    // 言い換えると、Int型の"値が有るかもしれないし無いかもしれない"という場面を想定します。
+    // まず、4-1で登場したList型に登場してきてもらいます。
+    val nonEmptyNums: List[Int] = List(1, 2, 3)
+    val emptyNums: List[Int] = List() // 空です。
+
+    // 次に、これらのリストの先頭の値を取得したかったとします。
+    // nonEmptyNums(0) // `nonEmptyNums.head`というメソッドでも同じことができます。
+    // emptyNums(0) // `emptyNums.head`というメソッドでも同じことができます。
+    // とかけば目的が達成できそうではあるのですが、
+    // emptyNumsは空なので、実行時にエラーが発生してしまいます。
+    // より安全な実装をするための方法の1つとしてOption型を利用できます。
+    // List型を受け取って先頭の値があればそれを返却するOption型を利用した関数の例が以下です。
+    def headOption(nums: List[Int]): Option[Int] = {
+      // ListのnonEmptyメソッドは値が1つでも入っていればtrueになります。
+      if (nums.nonEmpty) Some(nums.head) // `Some(値)`でSome型の値が生成できます。
+      else None // Noneは値を含まないのでシンプルに`None`とだけ書きます。
+    }
+
+    // 注目点としまして、返り値の型がOption[Int]型になっています。
+    // 6-8の内容になりますが、子クラスは親クラスの変数や関数の返り値の型の値として利用できます。
+    // また、Option自体は抽象クラスなので、
+    // この関数から返却される実際の値はSome型かNone型だということになります。
+    // この関数では、Listが空でないときにだけ`head`メソッドを呼び出しているので、
+    // 実行時にエラーになることが無い、安全な実装になっています。
+    // 実際に利用する時はmatchと組合せて以下のように使うことができます。
+    val numsHead1 = headOption(nonEmptyNums)
+    val numsHead1AsString = numsHead1 match {
+      // Some(変数名)をcaseに指定することでSome型だった場合に変数に中身の値を格納できます。
+      case Some(n) => s"numsHead1はSomeで${n}が入っていました。"
+      case None => "numsHead1はNoneでした。"
     }
     
-    val sg = new SomethingGreat()
-    val sgAsString = sg.toString()
-    val sgAsStringByObject = sg.toStringByObject()
-    
+    val numsHead2 = headOption(emptyNums)
+    val numsHead2AsString = numsHead2 match {
+      case Some(n) => s"numsHead2はSomeで${n}が入っていました。"
+      case None => "numsHead2はNoneでした。"
+    }
+
+    // match式で型をチェックしている分、
+    // ただただheadを呼び出すよりは複雑になりますが、
+    // 実行時にエラーにならない安全な実装というのは、
+    // とてつもなく重要だと考えています。
+    // "値が有るかもしれないし無いかもしれない"という場面はよく遭遇するため、
+    // この状況を安全に実装できるOption型は重宝されているわけです。
+
     Ok(
       s"""
-         |sgAsString = $sgAsString<br>
-         |sgAsStringByObject = $sgAsStringByObject<br>
+         |$numsHead1AsString<br>
+         |$numsHead2AsString<br>
          |""".stripMargin).as(HTML)
+    
+    // ちなみにちなみに、
+    // 今回実装したheadOption関数は、
+    // 実はListの組込メソッドとして既に用意されていまして、
+    // 名前は同じheadOptionです。
+    // nonEmptyNums.headOption
+    // みたいに使えます。
+    // このメソッドは、今回実装したList[Int]型専用ではなく、
+    // 任意の型を含むListで利用できます。
   }
 
 }
