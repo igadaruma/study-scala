8e3ed49111955ff403fe361f25b30c434dbef710
Shogo Kagenaga <s.kagenaga@gmail.com>
2022-01-31 23:08:18 +0900

    WIP: chapter2

--- app/controllers/chapter2/Chapter2_0Controller.scala
+++ app/controllers/chapter2/Chapter2_0Controller.scala
@@ -8,9 +8,16 @@ import javax.inject._
 class Chapter2_0Controller @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
   def chapter2_0 = Action { _ =>
-    // 今は細かいことは気にせず、
-    // これでブラウザに文字が出るんだと納得ください。
-    // 約束だぞ🥺
+    // <- この記号から始まる行はコメントといって、
+    // プログラムにはなんの影響も及ぼさないものになっています。
+    
+    // 今後もこのソースのようにサンプルプログラムと、
+    // 色々説明を記載させていただきます。
+    
+    // まずここでは、意味不明だと思いますが、
+    // 以下のような`Ok()`というプログラムで、
+    // ブラウザにHTMLが返って画面に表示されるんだということをご納得くださいませ。約束だぞ🥺
+    
     Ok("ここにはHTMLを書きます。").as(HTML)
   }
 
